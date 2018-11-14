4cee63577da9827bc6d93da8743edacc3f6b1067
Benjamin Petit <petit.benjam@gmail.com>
2018-11-14 14:04:54 -0800

    Created image_completion

---
+++ image_completion.py
@@ -0,0 +1,24 @@
+import torch
+import torch.utils.data
+from torchvision import datasets, transforms
+from torchvision.utils import save_image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import time
+
+from NP import ContextEncoder, ContextToDistrib, Decoder
+
+def complete_full_image(decoder, context):
+    return None
+
+def complete_pixel(decoder, context, pixel):
+    return None
+
+def main():
+    c
+    return
+
+if __name__ == "main":
+
+    main()
