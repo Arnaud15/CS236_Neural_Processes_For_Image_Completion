43da3a7b0e300b6ec1fd673fc7d38d485c88aa8e
Amaury Sabran <amaury_sabran@live.fr>
2018-11-15 18:19:31 -0800

    complete image

--- complete_image.py
+++ complete_image.py
@@ -13,6 +13,12 @@ def random_mask(bsize, n_pixels_to_keep, total_pixels=784):
     return torch.tensor(mask).float()
 
 
+def upper_half_mask(bsize, n_rows=14, h=28, w=28):
+    mask = np.zeros((bsize, h, w))
+    mask[:, :n_rows, :] = 1
+    return torch.tensor(mask).float().view(bsize, -1)
+
+
 def display_images(original_image, mask, reconstructed_image, h=28, w=28):
     bsize = original_image.size(0)
     original_image = original_image.view(bsize, 1, h, w).expand(-1, 3, -1, -1)
@@ -29,7 +35,8 @@ def display_images(original_image, mask, reconstructed_image, h=28, w=28):
     return np.transpose(grid.detach().numpy(), (1, 2, 0))
 
 
-def get_sample_images(batch, h, w, context_encoder, context_to_dist, decoder, n_pixels, n_samples, save=False,
+def get_sample_images(batch, h, w, context_encoder, context_to_dist, decoder, n_pixels, n_samples, mask=None,
+                      save=False,
                       save_file=""):
     # make grid
     device = batch.device
@@ -37,7 +44,10 @@ def get_sample_images(batch, h, w, context_encoder, context_to_dist, decoder, n_
     context_full = context_encoder(torch.cat([batch, grid], dim=-1))
 
     # sample random indices to keep
-    mask = random_mask(batch.size(0), n_pixels, total_pixels=784).to(device)
+    if mask is None:
+        mask = random_mask(batch.size(0), n_pixels, total_pixels=784).to(device)
+    else:
+        mask = mask.to(device)
     context_masked = (context_full * mask.unsqueeze(-1)).sum(dim=1) / mask.sum(dim=1, keepdim=True)
     z_context = torch.cat(
         [sample_z(context_to_dist(context_masked)).unsqueeze(1).expand(-1, h * w, -1) for i in range(n_samples)],
@@ -79,15 +89,22 @@ def main(args):
     batch, _ = next(iter(test_loader))
     batch = batch.view(batch.size(0), -1, 1).to(device)  # bsize * 784 *1
 
-    get_sample_images(batch, h, w, context_encoder, context_to_dist, decoder, args.n_pixels, args.n_samples, save=False)
+    if args.mask_type == "upper":
+        mask = upper_half_mask(args.bsize, n_rows=14, h=h, w=w)
+    else:
+        assert args.mask_type == 'random'
+        mask = random_mask(batch.size(0), args.n_pixels, total_pixels=784)
+
+    get_sample_images(batch, h, w, context_encoder, context_to_dist, decoder, args.n_pixels, args.n_samples, mask=mask,
+                      save=False)
 
 
 parser = ArgumentParser()
 parser.add_argument("--resume_file", type=str, default="models/NP_model_epoch_5000.pt")
 parser.add_argument("--bsize", type=int, default=10)
-parser.add_argument("--n_pixels", type=int, default=100)
+parser.add_argument("--n_pixels", type=int, default=50)
 parser.add_argument("--n_samples", type=int, default=5, help="number of samples per context point")
-
+parser.add_argument("--mask_type", type=str, choices=["random", "upper"], default="random")
 if __name__ == '__main__':
     args = parser.parse_args()
     main(args)
