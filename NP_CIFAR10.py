7b38710c9a07df3bf3e17047f6611143f4681648
Arnaud15 <arnaud.autef@gmail.com>
2018-12-06 21:12:27 -0800

    true CIFAR10 upload

--- NP_CIFAR10.py
+++ NP_CIFAR10.py
@@ -5,34 +5,63 @@ from torch.nn import functional as F
 from torchvision import datasets, transforms
 import time
 from argparse import ArgumentParser
-from modelsCIFAR import *
-from utils import *
+from models import *
+import os
+from utils import sample_z, log_normal, make_mesh_grid, kl_normal, save_model, random_mask_uniform, random_mask, display_images, \
+    load_models
 from tensorboardX import SummaryWriter
-from complete_image import get_sample_images, random_mask
 from math import pi as PI
 
 
-def log_normal(x, m, v):
-    """
-    Computes the elem-wise log probability of a Gaussian and then sum over the
-    last dim. Basically we're assuming all dims are batch dims except for the
-    last dim.
+def all_forward(batch, grid, mask, context_encoder, aggregator, context_to_dist):
+    '''
+    :param batch: input batch of size bsize,c,h,w
+    :param grid: grid of size h,w,2
+    :param mask: mask of size bsize,h,w
+    :param context_encoder: model (2+c -> d)
+    :param aggregator: aggregates mask and context encodings (k*d ->d)
+    :param context_to_dist: encoded context to z distribution
+    :return: z_params_full,z_params_masked of shape (bsize,d), (bsize,d) where d is the hidden dimension of the context
+    '''
+    bsize, c, h, w = batch.size()
 
-    Args:
-        x: tensor: (batch, ..., dim): Observation
-        m: tensor: (batch, ..., dim): Mean
-        v: tensor: (batch, ..., dim): Variance
-    """
-    log_prob = (-0.5 * (x - m).pow(2) / v -0.5 * torch.log( 2 * PI * v) ).sum( dim=-1)
-    return log_prob
+    batch = batch.view(bsize, c, h * w).transpose(1, 2)  # bsize,h*w,c
+    grid = grid.view(h * w, -1).unsqueeze(0).expand(bsize, -1, -1)  # bsize,h*w,2
+    context_data = torch.cat([batch, grid], dim=2)
 
+    context_full = context_encoder(context_data)  # bsize,h*w,d
+
+    mask = mask.view(bsize, h * w, 1)
+    r_masked = aggregator(context_full, mask=mask, agg_dim=1)  # bsize * hidden_size
+    r_full = aggregator(context_full, mask=None, agg_dim=1)
+
+    z_params_masked = context_to_dist(r_masked)
+    z_params_full = context_to_dist(r_full)
+
+    return z_params_full, z_params_masked
+
+def compute_loss(batch, grid, mask, z_params_full, z_params_masked, h, w, decoder):
+    ## compute loss
+    z_full = sample_z(z_params_full)  # size bsize * hidden
+    z_full = z_full.unsqueeze(1).expand(-1, h * w, -1)
+
+    # resize context to have one context per input coordinate
+    grid_input = grid.view(1, h * w, -1).expand(batch.size(0), -1, -1)
+    target_input = torch.cat([z_full, grid_input], dim=-1)
+
+    reconstructed_image_mean, reconstructed_image_variance = decoder(target_input)  # bsize,h*w,1
+
+    reconstruction_loss = - (log_normal(batch.view(batch.size(0), h * w, 3), reconstructed_image_mean, reconstructed_image_variance) * (1 - mask.view(-1, h * w, 1))).sum(dim=1).mean()
+
+    kl_loss = kl_normal(z_params_full, z_params_masked).mean()
+    return reconstruction_loss, kl_loss, reconstructed_image_mean, reconstructed_image_variance
 
 def train(context_encoder, context_to_dist, decoder, aggregator, train_loader, test_loader, optimizer, n_epochs, device,
           save_path,
           summary_writer, save_every=10, h=32, w=32, log=1):
     context_encoder.train()
     decoder.train()
-    grid = make_mesh_grid(h, w).to(device).view(h * w, 2)  # size 784*2
+    grid = make_mesh_grid(h, w).to(device).view(h * w, 2)  # size 1024*2
 
     for epoch in range(n_epochs):
         running_loss = 0.0
@@ -50,46 +79,22 @@ def train(context_encoder, context_to_dist, decoder, aggregator, train_loader, t
                 last_log_time = time.time()
                 running_loss = 0.0
 
-            mask = random_mask_uniform(batch_shape=(batch.size(0), h * w), device=batch.device, h=h, w=w)
-            context_data = torch.cat(
-                #MODIF HERE
-                [batch.view(batch.size(0), h * w, 3), grid.unsqueeze(0).expand(batch.size(0), h * w, 2)], dim=-1)
+            mask = random_mask_uniform(bsize=batch.size(0), h=h, w=w, device=batch.device)
+            z_params_full, z_params_masked = all_forward(batch, grid, mask, context_encoder, aggregator,
+                                                         context_to_dist)
 
-            # context data size (bsize,h*w,5) with 5 = (pixel valueR, pix_valueG, pix_valueB, coord_x,coord_y)
-
-            context_full = context_encoder(context_data)  # size bsize,h*w,d with d =hidden size
-
-            mask = mask.unsqueeze(-1)
-            r_masked = aggregator.forward(context_full, mask=mask, agg_dim=1)  # bsize * hidden_size
-            r_full = aggregator.forward(context_full, mask=None, agg_dim=1)
-            # print("relative diff between masked and full {:.2f}".format(torch.norm(r_masked-r_full)/torch.norm(r_full)))
-
-            ## compute loss
-            z_params_full = context_to_dist(r_full)
-            z_params_masked = context_to_dist(r_masked)
-            z_full = sample_z(z_params_full)  # size bsize * hidden
-            z_full = z_full.unsqueeze(1).expand(-1, h * w, -1)
-
-            # resize context to have one context per input coordinate
-            grid_input = grid.unsqueeze(0).expand(batch.size(0), -1, -1)
-            target_input = torch.cat([z_full, grid_input], dim=-1)
-            #MODIF HERE
-            reconstructed_image_mean, reconstructed_image_variance = decoder.forward(target_input)
+            reconstruction_loss, kl_loss, reconstructed_image_mean, reconstructed_image_variance = compute_loss(batch, grid, mask, z_params_full,
+                                                                             z_params_masked, h, w,
+                                                                             decoder)
+            loss = reconstruction_loss + kl_loss
             # if batch_idx == 0 and log:
             #     if not os.path.exists("images"):
             #         os.makedirs("images")
             #     save_images_batch(batch.cpu(), "images/CIFAR_target_epoch_{}".format(epoch), h=h, w=w)
             #     save_images_batch(reconstructed_image_mean.cpu(), "images/CIFAR_reconstruct_epoch_{}".format(epoch), h=h, w=w)
-
-            #MODIFS HERE
-            mask = mask.squeeze(-1)
-            reconstruction_loss = - (log_normal(batch.view(batch.size(0), h * w, 3), reconstructed_image_mean, reconstructed_image_variance) * (1 - mask)).sum(dim=1).mean()
-
-            kl_loss = kl_normal(z_params_full, z_params_masked).mean()
             if batch_idx % 100 == 0:
                 print("reconstruction {:.2f} | kl {:.2f}".format(reconstruction_loss, kl_loss))
 
-            loss = reconstruction_loss + kl_loss
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -107,62 +112,48 @@ def train(context_encoder, context_to_dist, decoder, aggregator, train_loader, t
                        device)
         #Testing
         test_loss = 0.0
-        with torch.no_grad():
+        
             for batch_idx, (batch, _) in enumerate(test_loader):
+
                 batch = batch.to(device)
+                mask = random_mask_uniform(bsize=batch.size(0), h=h, w=w, device=batch.device)
 
-                mask = random_mask_uniform(batch_shape=(batch.size(0), h * w), device=batch.device, h=h, w=w)
+                with torch.no_grad():
+                    z_params_full, z_params_masked = all_forward(batch, grid, mask, context_encoder, aggregator,
+                                                                 context_to_dist)
+                    reconstruction_loss, kl_loss, reconstructed_image_mean, reconstructed_image_variance = compute_loss(batch, grid, mask, z_params_full,
+                                                                                     z_params_masked, h, w,
+                                                                                     decoder)
+                    loss = reconstruction_loss + kl_loss
+                    test_loss += loss.item()
 
-                context_data = torch.cat(
-                    [batch.view(batch.size(0), h * w, 3), grid.unsqueeze(0).expand(batch.size(0), h * w, 2)], dim=-1)
-                # context data size (bsize,h*w,5) with 5 = (pixel valueR, pix_valueG, pix_valueB, coord_x,coord_y)
-
-                context_full = context_encoder(context_data)  # size bsize,h*w,d with d =hidden size
-
-                mask = mask.unsqueeze(-1)  # size bsize * 784 *
-                r_masked = aggregator.forward(context_full, mask=mask, agg_dim=1)  # bsize * hidden_size
-                r_full = aggregator.forward(context_full, mask=None, agg_dim=1)
-                # print("relative diff between masked and full {:.2f}".format(torch.norm(r_masked-r_full)/torch.norm(r_full)))
-
-                ## compute loss
-                z_params_full = context_to_dist(r_full)
-                z_params_masked = context_to_dist(r_masked)
-                z_full = sample_z(z_params_full)  # size bsize * hidden
-                z_full = z_full.unsqueeze(1).expand(-1, h * w, -1)
-
-                # resize context to have one context per input coordinate
-                grid_input = grid.unsqueeze(0).expand(batch.size(0), -1, -1)
-                target_input = torch.cat([z_full, grid_input], dim=-1)
-
-                #MODIF HERE
-                reconstructed_image_mean, reconstructed_image_variance = decoder.forward(target_input)
-                mask = mask.squeeze(-1)
-                reconstruction_loss = - (log_normal(batch.view(batch.size(0), h * w, 3), reconstructed_image_mean, reconstructed_image_variance) * (1 - mask)).sum(dim=1).mean()
-
-                kl_loss = kl_normal(z_params_full, z_params_masked).mean()
-                loss = reconstruction_loss + kl_loss
-                test_loss += loss.item()
             if summary_writer is not None:
-                summary_writer.add_scalar("test/loss", test_loss / len(test_loader), global_step=epoch)
+            summary_writer.add_scalar("test/loss", test_loss / len(test_loader), global_step=epoch)
 
-            # do examples
+        # do examples
+        #TODO CIFAR IMAGE
+        # example_batch, _ = next(iter(test_loader))
+        # example_batch = example_batch[:10]
+        # for n_pixels in [50, 150, 450]:
+        #     mask = random_mask(example_batch.size(0), h, w, n_pixels, device=example_batch.device)
+        #     z_params_full, z_params_masked = all_forward(example_batch, grid, mask, context_encoder, aggregator,
+        #                                                  context_to_dist)
 
-            test_batch, _ = next(iter(test_loader))
-            test_batch = test_batch[:10]
-            test_batch = test_batch.view(test_batch.size(0), -1, 1).to(device)  # bsize * 784 *1
-            # for n_pixels in [50, 150, 450]:
-            #     mask = random_mask(test_batch.size(0), n_pixels, total_pixels=784)
+        #     z_context = torch.cat(
+        #         [sample_z(context_to_dist(z_params_masked)).unsqueeze(1).expand(-1, h * w, -1) for i in
+        #          range(3)],
+        #         dim=0)
+        #     decoded_images = decoder(z_context).view(example_batch.size(0), h, w)
+        #     import pdb;
+        #     pdb.set_trace()
+        #     stacked_images = display_images(original_image=example_batch, mask=mask, reconstructed_image=decoded_images)
 
-            # image = get_sample_images(test_batch, h, w, context_encoder, context_to_dist, decoder, n_pixels, 4,
-            #                           mask=mask,
-            #                           save=False)
-            # image = torch.tensor(image).transpose(0, 2).unsqueeze(0).transpose(2, 3)
+        #     image = torch.tensor(image).transpose(0, 2).unsqueeze(0).transpose(2, 3)
 
-            # import pdb;
-            # # pdb.set_trace()
-            # if summary_writer is not None:
-            #     summary_writer.add_image("test_image/{}_pixels".format(n_pixels), image, global_step=epoch)
-
+        #     # import pdb;
+        #     # pdb.set_trace()
+        #     if summary_writer is not None:
+        #         summary_writer.add_image("test_image/{}_pixels".format(n_pixels), image, global_step=epoch)
     return
 
 
@@ -192,9 +183,9 @@ def main(args):
         ])),
         batch_size=args.bsize, shuffle=True)
 
-    context_encoder = ContextEncoder()
+    context_encoder = ContextEncoderCIFAR()
     context_to_dist = ContextToLatentDistribution()
-    decoder = Decoder()
+    decoder = DecoderCIFAR()
 
     if args.resume_file is not None:
         load_models(args.resume_file, context_encoder, context_to_dist, decoder)
